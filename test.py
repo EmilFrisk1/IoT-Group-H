969d02952a5050abf265f907b0b40454e4dcf35a
EmilFrisk1 <friskemil2@gmail.com>
2025-03-16 15:59:12 +0200

    mhm

--- test.py
+++
@@ -1,9 +0,0 @@
-import re
-
-connection_string = "HostName=anemoiahub.azure-devices.net;DeviceId=testidevice;SharedAccessKey=ar/pbGoqZe47Qs5uLhZgDEuO+AFwLyLM0tlmyVKQRqg="
-match = re.search(r'DeviceId=([^;]+);', connection_string)
-device_id = match.group(1) if match else "Not found"
-
-print(device_id)  # Output: asdsaoskdja
-
-
